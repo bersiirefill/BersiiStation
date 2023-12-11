6ef7e509061a3d3d64d2866138f7b43eb4290533
bersiirefill <bersiirefill@gmail.com>
2023-12-12 01:54:43 +0700

    Update ke versi terbaru

--- index.py
+++ index.py
@@ -1,27 +1,41 @@
 '''
-Bersii Refill Station - V1.0
-Code created by Matt Richardson 
-Modified by Hanustavira Guru Acarya
-for details, visit:  http://mattrichardson.com/Raspberry-Pi-Flask/inde...
+Bersii Refill Station - V2.0 / Juni 2023
 '''
+import base64
 import requests
 import json
+import time
+import subprocess
 import RPi.GPIO as GPIO
-from flask import Flask, request, render_template, session, url_for, redirect
-
+from flask import Flask, request, jsonify, render_template, session, url_for, redirect
+from flask_apscheduler import APScheduler
+from datetime import datetime
+# from apscheduler.schedulers.background import BackgroundScheduler
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+# Inisialisasi scheduler
+scheduler = APScheduler()
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 # Atur pin GPIO mesin refill
-refill = 12
+mesin_pin = 26
+trigpin = 17
+echopin = 27
+refill = [mesin_pin, trigpin]
 # Atur status mesin refill
 refillSts = 0
 # Atur pin GPIO mesin refill sebagai output
-GPIO.setup(refill, GPIO.OUT) 
+for pin in refill:
+    GPIO.setup(pin, GPIO.OUT)
+# Atur pin GPIO Ultrasonic
+GPIO.setup(echopin, GPIO.IN)
 # Matikan mesin refill saat rpi menyala 
 GPIO.output(refill, GPIO.LOW)
+
+# Kalibrasi
+json_file_path = "config.json"
 
 # Serial Number - Nomor Seri
 def getserial():
@@ -36,24 +50,26 @@ def getserial():
   except:
     cpuserial = "ERROR000000000"
   return cpuserial
+  
+def getipaddress():
+   ipaddr = subprocess.getoutput('hostname -I').strip()
+   return ipaddr
 
-def revoke_token(sessions, status):
+def revoke_token(sessions):
    ssn_id = sessions.split('|')[0]
    # Data yang dikirim ke API
    serial = {'nomor_seri':getserial(), 'token_id':ssn_id}
    # Kirim POST ke server dan simpan sebagai object
-   tokenizes_r = requests.post(url = "http://192.168.1.70:8000/api/revoke_station", data = serial, headers = {"Accept": "application/json", "Authorization": "Bearer " + sessions})
+   tokenizes_r = requests.post(url = "https://bersii.my.id/api/revoke_station", data = serial, headers = {"Accept": "application/json", "Authorization": "Bearer " + sessions})
    loads = tokenizes_r.json()
    message = loads["message"]
    if message == "Token Dicabut":
-      if status == "user":
-         session.pop("token", None)
-      else:
-         session.pop("id_admin", None)
-         session.pop("nama_admin", None)
-         session.pop("email_admin", None)
-         session.pop("jabatan_admin", None)
-         session.pop("token_admin", None)
+      session.pop("id_admin", None)
+      session.pop("nama_admin", None)
+      session.pop("email_admin", None)
+      session.pop("jabatan_admin", None)
+      session.pop("token_admin", None)
+      session.clear()
    else:
       print("Token Gagal Dicabut")
    return message
@@ -62,141 +78,305 @@ def revoke_all():
    # Data yang dikirim ke API
    serial = {'nomor_seri':getserial()}
    # Kirim POST ke server dan simpan sebagai object
-   tokenizes_r = requests.post(url = "http://192.168.1.70:8000/api/revoke_all", data = serial, headers = {"Accept": "application/json"})
+   tokenizes_r = requests.post(url = "https://bersii.my.id/api/revoke_all", data = serial, headers = {"Accept": "application/json"})
    loads = tokenizes_r.json()
    message = loads["message"]
    if message == "Token Dicabut":
-      session.pop("token", None)
+      session.pop("id_admin", None)
+      session.pop("nama_admin", None)
+      session.pop("email_admin", None)
+      session.pop("jabatan_admin", None)
       session.pop("token_admin", None)
+      session.clear()
+      print("Semua Token Dicabut")
    else:
       print("Token Gagal Dicabut")
    return message
 
-@app.route("/<action>")
-def action(action):
-   if action == "on":
-      GPIO.output(refill, GPIO.HIGH)
-   if action == "off":
-      GPIO.output(refill, GPIO.LOW)
-   refillSts = GPIO.input(refill)
-   templateData = {
-      'status' : refillSts,
-   }
-   return render_template('index.html', **templateData)
-
-# Bagian Admin
-@app.route("/hardware_admin")
-def hardware_admin():
-   f = open('/proc/cpuinfo','r')
-   templateData = {
-      'hardware' : f,
-   }
-   return render_template('hardware_admin.html', **templateData)
-
-@app.route("/index_admin")
-def index_admin():
+@app.route("/logout", methods =["GET", "POST"])
+def logout():
    admin_ssn = session["token_admin"]
-   # Data yang dikirim ke API
-   data = {'nomor_seri':getserial()}
-   # Kirim POST ke server dan simpan sebagai object
-   rqs = requests.post(url = "http://192.168.1.70:8000/api/station_stock", data = data, headers = {"Accept": "application/json", "Authorization": "Bearer " + admin_ssn})
-   rds = rqs.json()
-   nama_produk = rds["data"][0]["nama_produk"]
-   stok_produk = rds["data"][0]["stok"]
-   templateData = {
-      'nama_produk' : nama_produk,
-      'stok_produk' : stok_produk,
-   }
-   return render_template('index_admin.html', **templateData)
-   
-@app.route("/hwstatus_admin", methods =["GET", "POST"])
-def hwstatus_admin():
-   admin_ssn = session["token_admin"]
+   revoke = revoke_token(admin_ssn)
+   return revoke
+
+@app.route("/")
+def start():
+   return redirect(url_for("master"))
+
+@app.route("/index", methods =["GET", "POST"])
+def master():
+   admin_ssn = None
    if request.method == "POST":
-      stat = request.form.get("stat")
+      # Baca input form HTML
+      email = request.form.get("email")
+      password = request.form.get("password")
       # Data yang dikirim ke API
-      data = {'nomor_seri':getserial(), 'status':stat}
+      data = {'email':email, 'password':password, 'nomor_seri':getserial()}
       # Kirim POST ke server dan simpan sebagai object
-      rqs = requests.post(url = "http://192.168.1.70:8000/api/change_station_status", data = data, headers = {"Accept": "application/json", "Authorization": "Bearer " + admin_ssn})
+      r = requests.post(url = "https://bersii.my.id/api/login_station_admin", data = data, headers = {"Accept": "application/json"})
+      loads = r.json()
+      # Fetch
+      id_admin = loads["data"]["id_admin"]
+      nama_admin = loads["data"]["nama"]
+      email_admin = loads["data"]["email"]
+      jabatan_admin = loads["data"]["jabatan"]
+      token_admin = loads["data"]["token"]
+      # Masukkan ke session
+      session["id_admin"] = id_admin
+      session["nama_admin"] = nama_admin
+      session["email_admin"] = email_admin
+      session["jabatan_admin"] = jabatan_admin
+      session["token_admin"] = token_admin
+      # Render halaman dashboard
+      admin_ssn = session["token_admin"]
+      # Data yang dikirim ke API
+      data = {'nomor_seri':getserial()}
+      # Kirim POST ke server dan simpan sebagai object
+      rqs = requests.post(url = "https://bersii.my.id/api/station_stock", data = data, headers = {"Accept": "application/json", "Authorization": "Bearer " + admin_ssn})
       rds = rqs.json()
-      msg = rds["message"]
-      return redirect(url_for("hwstatus_admin"))
-      
-   # Data yang dikirim ke API
-   data = {'nomor_seri':getserial()}
-   # Kirim POST ke server dan simpan sebagai object
-   r = requests.post(url = "http://192.168.1.70:8000/api/station_status", data = data, headers = {"Accept": "application/json", "Authorization": "Bearer " + admin_ssn})
-   loads = r.json()
-   templateData = {
-      'nomor_seri' : getserial(),
-      'latitude' : loads["data"]["latitude"],
-      'longitude' : loads["data"]["longitude"],
-      'status_mesin' : loads["data"]["status_mesin"],
-      'alamat' : loads["data"]["alamat"],
-      'update_terakhir' : loads["data"]["update_terakhir"],
-   }
-   return render_template('hwstatus_admin.html', **templateData)
-
-@app.route("/login_admin", methods =["GET", "POST"])
-def login_admin():
-   if request.method == "POST":
-       # Baca input form HTML
-       email = request.form.get("email")
-       password = request.form.get("password")
-       # Data yang dikirim ke API
-       data = {'email':email, 'password':password, 'nomor_seri':getserial()}
-       # Kirim POST ke server dan simpan sebagai object
-       r = requests.post(url = "http://192.168.1.70:8000/api/login_station_admin", data = data, headers = {"Accept": "application/json"})
-       loads = r.json()
-       # Fetch
-       id_admin = loads["data"]["id_admin"]
-       nama_admin = loads["data"]["nama"]
-       email_admin = loads["data"]["email"]
-       jabatan_admin = loads["data"]["jabatan"]
-       token_admin = loads["data"]["token"]
-       # Masukkan ke session
-       session["id_admin"] = id_admin
-       session["nama_admin"] = nama_admin
-       session["email_admin"] = email_admin
-       session["jabatan_admin"] = jabatan_admin
-       session["token_admin"] = token_admin
-       return redirect(url_for("index_admin"))
-   return render_template("login_admin.html")
-   
-@app.route("/logout_admin", methods =["GET", "POST"])
-def logout_admin():
-   admin_ssn = session["token_admin"]
-   revoke = revoke_token(admin_ssn, "admin")
-   if revoke == "Token Dicabut":
-      return redirect(url_for("login_admin"))
+      nama_produk = rds["data"]["nama_produk"]
+      stok_produk = rds["data"]["stok"]
+      harga_produk = rds["data"]["harga_produk"]
+      templateData = {
+         'nama_produk' : nama_produk,
+         'stok_produk' : stok_produk,
+         'harga_produk' : harga_produk,
+      }
+      return render_template('index.html', **templateData)
+   if "token_admin" in session:
+         # Data yang dikirim ke API
+         data = {'nomor_seri':getserial()}
+         # Kirim POST ke server dan simpan sebagai object
+         admin_ssn = session["token_admin"]
+         rqs = requests.post(url = "https://bersii.my.id/api/station_stock", data = data, headers = {"Accept": "application/json", "Authorization": "Bearer " + admin_ssn})
+         rds = rqs.json()
+         nama_produk = rds["data"]["nama_produk"]
+         stok_produk = rds["data"]["stok"]
+         harga_produk = rds["data"]["harga_produk"]
+         templateData = {
+            'nama_produk' : nama_produk,
+            'stok_produk' : stok_produk,
+            'harga_produk' : harga_produk,
+         }
+         return render_template('index.html', **templateData)
    else:
-      return "Token Gagal Dicabut"
+      return render_template("login.html")   
+   
+# BATAS MAIN CONTROLLER 
 
-# Bagian User
-@app.route("/", methods =["GET", "POST"])
-def login():
-   if request.method == "POST":
-       # Baca input form HTML
-       email = request.form.get("email")
-       password = request.form.get("password")
-       # Data yang dikirim ke API
-       data = {'email':email, 'password':password, 'nomor_seri':getserial()}
-       # Kirim POST ke server dan simpan sebagai object
-       r = requests.post(url = "http://192.168.1.70:8000/api/login_station", data = data, headers = {"Accept": "application/json"})
-       loads = r.json()
-       messages = loads["data"]["token"]
-       session["token"] = messages
-       return messages
-   return render_template("login.html")
+# Dashboard
+
+@app.route('/dashboard', methods=['GET', 'POST'])
+def dashboard():
+   if request.method == 'POST' or request.method == 'GET':
+      return render_template('dashboard/dashboard.html')
+   
+@app.route('/produksi', methods=['GET', 'POST'])
+def produksi():
+   if request.method == 'POST' or request.method == 'GET':
+      return render_template('dashboard/produksi.html')
+
+@app.route("/realtime_stok", methods=["POST"])
+def realtime_stok():
+   pulse_start = 0
+   pulse_end = 0
+   pulse_duration = 0
+   # Cek jarak antara air dan sensor ultrasonik
+   GPIO.output(trigpin, GPIO.HIGH)
+   time.sleep(0.00001)
+   GPIO.output(trigpin, GPIO.LOW)
+   while GPIO.input(echopin) == GPIO.LOW:
+      pulse_start = time.time()
+   while GPIO.input(echopin) == GPIO.HIGH:
+      pulse_end = time.time()
+   pulse_duration = pulse_end - pulse_start
+   distance = pulse_duration * 17474
+   # Atur isi default tangki
+   stok_produk = 0
+   if(distance > 26):
+      stok_produk = 0
+   elif(distance < 26 and distance > 13):
+      stok_produk = 5
+   else:
+      stok_produk = 10
+   admin_ssn = session["token_admin"]
+   # Data yang dikirim ke API
+   data = {
+      'nomor_seri':getserial(),
+      'stok_produk':stok_produk,
+   }
+   # Kirim POST ke server dan simpan sebagai object
+   rqs = requests.post(url = "https://bersii.my.id/api/station_stock", data = data, headers = {"Accept": "application/json", "Authorization": "Bearer " + admin_ssn})
+   rds = rqs.json()
+   # print(rds)
+   stok_produkr = rds["data"]["stok"]
+   nama_produkr = rds["data"]["nama_produk"]
+   # Cek sinkron
+   rst = 0
+   if(stok_produkr == stok_produk):
+      rst = stok_produkr
+   else:
+      rst = stok_produk
+   templateData = {
+      'stok_produk' : rst,
+      'jarak' : round(distance),
+      'nama_produk' : nama_produkr,
+   }
+   return json.dumps(templateData)
+
+# Pembukuan
+
+@app.route('/pembukuan', methods=['GET', 'POST'])
+def pembukuan():
+   if request.method == 'POST' or request.method == 'GET':
+        return render_template('pembukuan/pembukuan.html')
+
+# Konfigurasi
+
+@app.route('/konfigurasi', methods=['GET', 'POST'])
+def konfigurasi():
+   if request.method == 'POST' or request.method == 'GET':
+         admin_ssn = session["token_admin"]
+         # Data yang dikirim ke API
+         data = {'nomor_seri':getserial()}
+         # Kirim POST ke server dan simpan sebagai object
+         r = requests.post(url = "https://bersii.my.id/api/station_status", data = data, headers = {"Accept": "application/json", "Authorization": "Bearer " + admin_ssn})
+         loads = r.json()
+         # Baca Config
+         with open(json_file_path, 'r') as file:
+            data = json.load(file)
+         kedalaman = data.get('kedalaman_max_cm')
+         kapasitas = data.get('kapasitas_max_liter')
+         konversi = data.get('konversi')
+         templateData = {
+            'nomor_seri' : getserial(),
+            'latitude' : loads["data"]["latitude"],
+            'longitude' : loads["data"]["longitude"],
+            'status_mesin' : loads["data"]["status_mesin"],
+            'alamat' : loads["data"]["alamat"],
+            'update_terakhir' : loads["data"]["update_terakhir"].split(".")[0],
+            'kedalaman' : kedalaman,
+            'kapasitas' : kapasitas,
+            'konversi' : konversi,
+         }
+         return render_template('konfigurasi/konfigurasi.html', **templateData)
+   
+@app.route('/save_konfigurasi', methods=['POST'])
+def save_konfigurasi():
+   admin_ssn = session["token_admin"]
+   status = request.form.get("status")
+   # Data yang dikirim ke API
+   data = {'nomor_seri':getserial(), 'status':status}
+   # Kirim POST ke server dan simpan sebagai object
+   rqs = requests.post(url = "https://bersii.my.id/api/change_station_status", data = data, headers = {"Accept": "application/json", "Authorization": "Bearer " + admin_ssn})
+   rds = rqs.json()
+   msg = rds["message"]
+   return msg
+
+@app.route("/hardware/<status>")
+def hardware(status):
+   if status == 'on':
+      GPIO.output(refill, GPIO.HIGH)
+      refillSts = GPIO.input(refill)
+      return refillSts
+   elif status == 'off':
+      GPIO.output(refill, GPIO.LOW)
+      refillSts = GPIO.input(refill)
+      return refillSts
+
+@app.route("/kalibrasi", methods=["POST"])
+def kalibrasi():
+   pulse_start = 0
+   pulse_end = 0
+   pulse_duration = 0
+   # Cek jarak antara air dan sensor ultrasonik
+   GPIO.output(trigpin, GPIO.HIGH)
+   time.sleep(0.00001)
+   GPIO.output(trigpin, GPIO.LOW)
+   while GPIO.input(echopin) == GPIO.LOW:
+      pulse_start = time.time()
+   while GPIO.input(echopin) == GPIO.HIGH:
+      pulse_end = time.time()
+   pulse_duration = pulse_end - pulse_start
+   distance = pulse_duration * 17474
+   templateData = {
+      'durasi' : round(pulse_duration),
+      'jarak' : round(distance),
+   }
+   return json.dumps(templateData)
+
+@app.route("/simpan_kalibrasi", methods=["POST"])
+def simpan_kalibrasi():
+   try:
+      # Baca input form HTML
+      kedalaman = request.form.get("kedalaman")
+      kapasitas = request.form.get("kapasitas")
+      konversi = request.form.get("konversi")
+      data = {
+         "kedalaman_max_cm": kedalaman,
+         "kapasitas_max_liter": kapasitas,
+         "konversi": konversi
+      }
+
+      # Save the data to the JSON file
+      with open(json_file_path, 'w') as file:
+         json.dump(data, file)
+
+      return jsonify({"message": "Data successfully saved."}), 200
+
+   except Exception as e:
+      return jsonify({"message": f"Error: {str(e)}"}), 500
+
+# Job terjadwal via cron
+@scheduler.task('interval', id='cron_stok', seconds=60)
+def cron_stok():
+   pulse_start = 0
+   pulse_end = 0
+   pulse_duration = 0
+   # Cek jarak antara air dan sensor ultrasonik
+   GPIO.output(trigpin, GPIO.HIGH)
+   time.sleep(0.00001)
+   GPIO.output(trigpin, GPIO.LOW)
+   while GPIO.input(echopin) == GPIO.LOW:
+      pulse_start = time.time()
+   while GPIO.input(echopin) == GPIO.HIGH:
+      pulse_end = time.time()
+   pulse_duration = pulse_end - pulse_start
+   distance = pulse_duration * 17474
+   # Atur isi default tangki
+   stok_produk = 0
+   if(distance > 26):
+      stok_produk = 0
+   elif(distance < 26 and distance > 13):
+      stok_produk = 5
+   else:
+      stok_produk = 10
+   # Data yang dikirim ke API
+   data = {
+      'nomor_seri':getserial(),
+      'stok_produk':stok_produk,
+   }
+   # Kirim POST ke server dan simpan sebagai object
+   rqs = requests.post(url = "https://bersii.my.id/api/cron_stok", data = data, headers = {"Accept": "application/json"})
+   rds = rqs.json()
+   current_datetime = datetime.now()
+   # print('Isi : ' + str(data["stok_produk"]) + ' / Jarak Pantul : ' + str(data["distance"]) + ' ------ ' + current_datetime.strftime("%Y-%m-%d %H:%M:%S")) 
+   status = rds["status"]
+   message = rds["message"]
+   print(' * ' + str(message) + ' --- (Stok : ' + str(stok_produk) + ' / Jarak Pantul : ' + str(distance) + ' --- ' + current_datetime.strftime("%Y-%m-%d %H:%M:%S"))
 
 if __name__ == "__main__":
    try:
-      print("Bersii Refill Station - V1.0")
+      ip = getipaddress()
+      print("Bersii Refill Station - V2.0")
       print(" * Nomor Seri : " + getserial())
-      app.run(host='192.168.1.90', port=80, debug=False)
+      print(" * Nomor IP : " + ip)
+      scheduler.init_app(app)
+      scheduler.start()
+      app.run(host=ip, port=6100, debug=False, ssl_context='adhoc')
       
    except KeyboardInterrupt:
       # Reset GPIO dan Token yang tersimpan
-      GPIO.cleanup()	
       revoke = revoke_all()
-   
+      GPIO.cleanup()	
+      
